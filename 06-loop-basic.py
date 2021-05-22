b5208ba495052d26df1ffb0b4f745065f87e8993
Jian-Lin Jiang <kobe24684@gmail.com>
2021-05-23 06:42:38 +0800

    lesson07

---
+++ 06-loop-basic.py
@@ -0,0 +1,27 @@
+# while 迴圈
+# 1+2+3+...+10
+n=1
+sum = 0 #用來記錄累加的結果
+while n <= 10:
+    sum = sum +n
+    n+=1
+print(sum)
+
+sum1 = 0
+for x in range(1,11):
+    sum1 = sum1 + x
+print(sum1)
+
+# for 迴圈
+# for x in [3, 5, 1]:
+#     print(x)
+
+# for x in "Hello":
+#     print(x)
+
+# for x in range(5):
+#     print(x)
+
+# for x in range(5,10):
+#     print(x)
+
